86c87c9685827b2026ee93c0c04f1e2b8852ff03
Unset <unset@email.example>
2024-09-23 02:10:23 +1000

    Comment example typed code

--- server/src/index.ts
+++ server/src/index.ts
@@ -37,12 +37,12 @@ async function fetchBalanceSheet() {
 	// Parse the Balance Sheet JSON with valibot so that we have typed runtime data
 	const parsedBalanceSheet = parseBalanceSheet(balanceSheetJSON);
 
-	// Now we can access this data in a typed way
-	const titles = parsedBalanceSheet.Reports.map((report) => {
-		return report.ReportTitles.join(", ");
-	});
+	// // Now we can access this data in a typed way, e.g.
+	// const titles = parsedBalanceSheet.Reports.map((report) => {
+	// 	return report.ReportTitles.join(", ");
+	// });
 
-	console.log({ titles });
+	// console.log({ titles });
 
 	return Response.json(parsedBalanceSheet);
 }
