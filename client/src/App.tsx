cca59af89eec463655007fa5bc77d2edec90f46c
Unset <unset@email.example>
2024-09-23 17:01:45 +1000

    Remove unused vite boilerplate

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,9 @@
-import { useEffect, useState } from "react";
-import viteLogo from "/vite.svg";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
 import {
 	type BalanceSheet,
 	parseBalanceSheet,
 } from "@show-me-the-money-code-test/common";
 
 function App() {
-	const [count, setCount] = useState(0);
 	const [balanceSheet, setBalanceSheet] = useState<BalanceSheet | null>(null);
 	const [parsingError, setParsingError] = useState<Error | null>(null);
 
@@ -30,26 +25,6 @@ function App() {
 		<>
 			{balanceSheet && <pre>{JSON.stringify(balanceSheet, null, 2)}</pre>}
 			{parsingError && <pre>{parsingError.message}</pre>}
-			<div>
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button type="button" onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
 		</>
 	);
 }
